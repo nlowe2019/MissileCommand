b8a3e635fed71f7c43734fab2c67a1b935de7da9
nlowe2019 <nplowe2000@hotmail.co.uk>
2019-05-12 16:59:24 +0100

    Player can fire projectiles created list to store active missiles missile will travel to cursor position when launched

--- Missile.c
+++ Missile.c
@@ -1,34 +1,54 @@
 #include <ncurses.h>
+#include <stdlib.h>
 
-#define WIDTH 256
-#define HEIGHT 64
+#define WIDTH 256       //width of window
+#define HEIGHT 64       //height of window
 
-typedef struct city {
+typedef struct city {   //cities to protect
     int id;
     int x;
     int y;
-    bool destroyed;
+    bool destroyed;     //has it been destoyed or not
 } city;
 
-typedef struct cursor {
-    int x;
-    int y;
+typedef struct cursor { //used to direct missiles
+    int x;              //x location on screen
+    int y;              //y ^
 } cursor;
 
-typedef struct battery {
+typedef struct battery { //
     int id;
-    int x;
+    int x;               //coordinates
     int y;
-    int stock;
-    bool destroyed;
+    int stock;           //remaining missiles
+    bool destroyed;      // has it been destoryed
 } battery;
 
-typedef struct missile {
-    int id;
-    int speed;
-    int x;
+typedef struct fmissile {   //player missiles
+    struct fmissile *next;
+    int speed;              //how fast
+    int x;                  //current coordinates
     int y;
-} missile;
+    int targetX;            //target coordinates
+    int targetY;
+} fmissile;
+
+typedef struct emissile {   //enemy missiles
+    struct emissile *next;
+    int speed;          
+    int x;              
+    int y;
+    int targetX;        
+    int targetY;
+} emissile;
+
+typedef struct emhead {     //enemy missiles list head
+    struct emissile *next;
+} emhead;
+
+typedef struct fmhead {     //friendly missiles list head
+    struct fmissile *next;
+} fmhead;
 
 void drawScene(WINDOW * win) {
     city cities[6];
@@ -41,17 +61,23 @@ void drawScene(WINDOW * win) {
 
     wattron(win, COLOR_PAIR(1));
     for(int i = 1; i < WIDTH-1; i++)
-        for(int j = 0; j < 2; j++)
+        for(int j = 0; j < 5; j++)
             mvwprintw(win, 2+j+(HEIGHT/8)*7, i, " ");
 
-    for(int i = 0; i < 3; i++) {
+    for(int i = 0; i < 1; i++) {
         batteries[i].id = i;
         batteries[i].y = (HEIGHT/8)*7;
         batteries[i].x = (WIDTH/7)*(i+1); 
         batteries[i].destroyed = false;
         batteries[i].stock = 10;
+
+        mvwprintw(win, batteries[i].y - 4, WIDTH/2 - 3, "         ");
+        mvwprintw(win, batteries[i].y - 3, WIDTH/2 - 4, "     Y     ");
+        mvwprintw(win, batteries[i].y - 2, WIDTH/2 - 5, "     Y Y     ");
+        mvwprintw(win, batteries[i].y - 1, WIDTH/2 - 6, "     Y Y Y     ");
+        mvwprintw(win, batteries[i].y - 0, WIDTH/2 - 7, "     Y Y Y Y     ");
+        mvwprintw(win, batteries[i].y + 1, WIDTH/2 - 7, "                 ");
     }
-    
     wattron(win, COLOR_PAIR(2));
     for(int i = 0; i < 6; i++) {
         cities[i].id = i;
@@ -77,6 +103,36 @@ WINDOW * gameWindow() {
     return win;
 }
 
+void launchMissile(WINDOW *win, fmhead *fmp, cursor *c) {
+    char k = wgetch(win);
+    if(k == 'q') {
+        fmissile *newmissile = (fmissile*)malloc(sizeof(fmissile));
+        newmissile->speed = 5;
+        newmissile->x = WIDTH/2;
+        newmissile->y = 7*(HEIGHT/8) - 10;
+        newmissile->targetX = c->x;
+        newmissile->targetY = c->y;
+        if(fmp->next != NULL)
+            newmissile->next = fmp->next;
+        fmp->next = newmissile;
+    }
+}
+
+void moveMissiles(WINDOW *win, fmhead *fmp) {
+    fmissile *node = fmp->next;
+    while(node != NULL) {
+        mvwprintw(win, node->y, node->x, " ");
+        if(node->y > node->targetY)
+            node->y -= node->speed;
+        if(node->x > node->targetX)
+            node->x -= node->speed;
+        else if(node->x < node->targetX)
+            node->x += node->speed;
+        mvwprintw(win, node->y, node->x, "Y");
+        node = node->next;
+    }
+}
+
 void moveCursor(cursor *pc, WINDOW *win) {
     int k = wgetch(win);
     wattron(win, COLOR_PAIR(4));
@@ -84,20 +140,20 @@ void moveCursor(cursor *pc, WINDOW *win) {
     wattron(win, COLOR_PAIR(3));
 		switch(k)
 		{	case KEY_UP:
-                        if((*pc).y > 3)
-                            (*pc).y -= 2;
+                        if(pc->y > 3)
+                            pc->y -= 2;
                         break;
             case KEY_DOWN:
-                        if((*pc).y < (HEIGHT/8)*7 - 5)
-                            (*pc).y += 2;
+                        if(pc->y < (HEIGHT/8)*7 - 8)
+                            pc->y += 2;
                         break;
             case KEY_LEFT:
-                        if((*pc).x > 6)
-                            (*pc).x -= 4;
+                        if(pc->x > 6)
+                            pc->x -= 4;
                         break;
             case KEY_RIGHT:
-                        if((*pc).x < WIDTH - 6)
-                            (*pc).x += 4;
+                        if(pc->x < WIDTH - 6)
+                            pc->x += 4;
                         break;
 		}
     mvwprintw(win, (*pc).y, (*pc).x, "+");
@@ -107,25 +163,30 @@ void moveCursor(cursor *pc, WINDOW *win) {
 
 int main() {
     
-    bool P = false;
-    cursor c;
-    c.x = 128;
+    bool P = false;    //is game paused
+    cursor c;          //creates cursor
+    c.x = 128;         //set coordiantes to middle of window
     c.y = 32;
-    cursor *pc = &c;
-    WINDOW *win;
+    cursor *pc = &c;   //pointer to cursor
+    WINDOW *win;       //pointer to window
+    fmhead *fhead = (fmhead*)malloc(sizeof(fmhead));
+    emhead *ehead = (emhead*)malloc(sizeof(emhead));
 
     initscr();
-    curs_set(0);
+    curs_set(0);                        //cursor invisible
     cbreak();
-    win = gameWindow(HEIGHT, WIDTH);
-    drawScene(win);
-    keypad(win, TRUE);
-    mousemask(ALL_MOUSE_EVENTS, NULL);
+    win = gameWindow(HEIGHT, WIDTH);    //creates window/border
+    drawScene(win);                     //sets initial stage
+    keypad(win, TRUE);                  //enables key input
+    mousemask(ALL_MOUSE_EVENTS, NULL);  //enable mouse input
 
-    while(!P) {
-        moveCursor(pc, win);
-        wrefresh(win);
+    while(!P) {                 //while unpaused
+        moveCursor(pc, win);    //update cursor
+        wrefresh(win);          //update window
+        launchMissile(win, fhead, pc);
+        moveMissiles(win, fhead); 
     }
+
 
     getch();
     endwin();
